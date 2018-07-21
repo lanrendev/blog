5dbb5f5a204a6720bb03b0b5c0887ac9c459873a
ricosmall <805163443@qq.com>
2018-07-21 18:29:34 +0800

    Update publish process

--- publish.sh
+++ publish.sh
@@ -4,5 +4,10 @@ git clone https://github.com/probberechts/hexo-theme-cactus.git --depth=1 themes
 # 替换主题配置文件
 cp ./_theme_config.yml ./themes/cactus/_confit.yml
 
-# 静态构建并部署
+# 静态构建并部署到 gh-pages
 hexo g -d
+
+# 源文件提交到 master 分支
+git add .
+git commit -m 'New post: $(date +%Y-%m-%d\ %H:%M:%S)'
+git push origin master
