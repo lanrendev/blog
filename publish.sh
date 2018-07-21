4230451b4475dbbc1b72080f16e9187db4c64f3e
ricosmall <805163443@qq.com>
2018-07-22 00:40:01 +0800

    New post: 2018-07-22 00:40:01

--- publish.sh
+++ publish.sh
@@ -1,13 +1,20 @@
 # 下载主题
 if [ ! -d 'themes/cactus' ]; then
     git clone https://github.com/probberechts/hexo-theme-cactus.git --depth=1 themes/cactus
+else
+    git pull
 fi
 
 # 替换主题配置文件
 cp ./_theme_config.yml ./themes/cactus/_config.yml
 
 # 静态构建并部署到 gh-pages
-hexo g -d
+if hash hexo 2>/dev/null; then
+    hexo g -d
+else
+    npm install hexo-cli -g
+    hexo g -d
+fi
 
 # 源文件提交到 master 分支
 git add .
